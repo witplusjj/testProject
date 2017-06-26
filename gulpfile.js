d492f920ec9749d825ceb21331d3e53e377a91bb
witplusjj <skyblue2394@witplus.com>
2017-06-26 18:20:21 +0900

    170626_history

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,39 @@
-var gulp = require("gulp");
+var gulp = require("gulp"),
+	webserver = require("gulp-webserver"),
+	sass = require("gulp-sass"),
+	livereload = require('gulp-livereload');
 
-gulp.task("default", function(){
-	console.log("test");
+var src = "project/src";
+var dist = "project/dist";
+
+// webserver 설정
+gulp.task('webserver', function() {
+	gulp.src(dist + '/')
+	.pipe(webserver({
+		livereload: true,
+		//port:8010,
+		//host:'192.168.11.172',
+		open:true
+	}));
 });
+
+// sass 설정 (sass 파일을 css 로 컴파일한다.)
+gulp.task("compile-sass", function(){
+	return gulp.src(src + "/scss/*.scss")
+		.pipe(sass()).on('error', sass.logError)
+		.pipe(gulp.dest(dist + "/assets/css"));
+});
+
+// 파일 변경 감지 및 브라우저 재시작
+gulp.task('watch', function () {
+	livereload.listen();
+	gulp.watch([src +"/scss/*.scss"], ['compile-sass']);
+	gulp.watch(dist + '/**').on('change', livereload.changed);
+});
+
+// default 설정
+gulp.task("default", [
+	"webserver",
+	"compile-sass",
+	"watch"
+]);
---
+++ project/dist/index.asp
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>테스트입니다.</title>
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="/assets/css/common.css" type="text/css" />
+</head>
+<body>
+<header id="header">
+	header 입니다.
+</header>
+<section id="contents" class="main">
+	test contents 입니다.
+	
+	<table>
+		<caption>test 표</caption>
+		<thead>
+			<tr>
+				<td>
+					test
+				</td>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>
+					test2111
+				</td>
+			</tr>
+			<tr>
+				<td>
+					test322
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	
+	<a href="#none">a href Link</a>
+</section>
+<footer id="footer">
+	footer 입니다.
+</footer>
+</body>
+</html>
